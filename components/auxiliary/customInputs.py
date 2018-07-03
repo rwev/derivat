cac6acaa0caebae26f7f25b60328d537334fe7c9
Ryan William <ryan.w.everett@gmail.com>
2018-07-04 01:45:05 +0200

    remove double definition

--- components/auxiliary/customInputs.py
+++ components/auxiliary/customInputs.py
@@ -76,17 +76,6 @@ class ParameterSelectionWidget(QtGui.QWidget):
 
 
             
-class Line(QtGui.QFrame):
-    def __init__(self, orientation = 'Horizontal' , parent = None):
-        QtGui.QFrame.__init__(self, parent)
-        if orientation == 'Horizontal': 
-            self.setFrameStyle(QtGui.QFrame.HLine)
-        elif orientation == 'Vertical':
-            self.setFrameStyle(QtGui.QFrame.VLine)
-        self.setFixedHeight(5)
-        self.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-
-
 
 
 
